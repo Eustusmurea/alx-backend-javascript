6ae58ae69e2f1e9b30f3e5e1082f943290081976
Eustus <mwirigieustus97@gmail.com>
2024-03-05 08:28:08 +0000

    Updates

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,20 +1,4 @@
 export function taskFirst() {
-<<<<<<< HEAD
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-=======
   const task = 'I prefer const when I can.';
   return task;
 }
@@ -29,4 +13,4 @@ export function taskNext() {
 
   return combination;
 }
->>>>>>> 0882398ca6ccc0d907ecb51d0acf3ffbeea7e4d9
+
--- 0x00-ES6_basic/babel.config.js
+++ 0x00-ES6_basic/babel.config.js
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
-module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
-      ],
-    ],
-  };
-=======
-const presets = [
-  [
-    '@babel/preset-env',
-    {
-      targets: {
-        node: 'current'
-      }
-    }
-  ]
-]
 
-module.exports = { presets }
->>>>>>> 0882398ca6ccc0d907ecb51d0acf3ffbeea7e4d9
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
