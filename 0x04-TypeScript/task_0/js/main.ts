19deef8c4a7b4bfd7396e6f8e18c42b7c6172177
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-03-24 23:24:34 +0300

    updates

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,48 +1,22 @@
-
-export interface Student {
-  firstName: string,
-  lastName: string,
-  age: number,
-  location: string,
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
 
-const student1: Student = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 25,
-  location: 'Nairobi',
+const stu1: Student = {
+  firstName: 'Eustus',
+  lastName: 'Mwirigi',
+  age: 26,
+  location: 'Kenya',
 };
 
-const student2: Student = {
-  firstName: 'Alex',
-  lastName: 'Sanchez',
-  age: 29,
-  location: 'Cairo',
+const stu2: Student = {
+  firstName: 'Emily',
+  lastName: 'Kanana',
+  age: 23,
+  location: 'Kenya'
 };
 
-const studentsList: Array<Student> = [
-  student1,
-  student2,
-];
-
-export const renderTable = (studentsList: Array<Student>): void =>  {
-  // create table tag
-  const table = document.createElement('table');
-  const headRow = document.createElement('tr');
-  table.insertAdjacentElement('beforeend', headRow);
-
-  // insert headers
-  headRow.insertAdjacentHTML('beforeend', '<th>FirstName</th>');
-  headRow.insertAdjacentHTML('beforeend', '<th>Location</th>');
-
-  for (const student of studentsList) {
-    const studentRow = document.createElement('tr')
-    studentRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
-    studentRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
-    table.insertAdjacentElement('beforeend', studentRow);
-  }
-
-  document.body.insertAdjacentElement('beforeend', table);
-}
-
-renderTable(studentsList);
+const StudentsList: Student[] = [stu1, stu2];
